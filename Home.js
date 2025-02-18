461f8e7ac8df696bf8704807b5b5ba829becb58f
Faheem jawaid <130732739+IamFJ03@users.noreply.github.com>
2025-02-18 13:14:48 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useContext} from 'react';
+import React,{useEffect, useState, useContext} from 'react';
 import {View, Text, StyleSheet, FlatList, Image, TouchableOpacity} from 'react-native';
 import {GlobalContext} from './Context/index';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -8,35 +8,56 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function Home({navigation}) {
 
   const {savedCity, setSavedCity} = useContext(GlobalContext);
-  useEffect(() => {
-   const loadCities = async () => {
+  const [loading, setLoading] = useState(true);
+
+  const fetchWeatherData = async (cities) =>{
+    try{
+       const updatedCities = await Promise.all(
+        cities.map(async (city) =>{
+          const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=02a4c8083ef436ed158568f05217cabd&units=metric`);
+          const data = await res.json();
+          return data;
+        })
+       );
+       setSavedCity(updatedCities);
+       await AsyncStorage.setItem('savedCity', JSON.stringify(updatedCities));
+    }
+    catch(error){
+      console.error('Error fetching weather data:', error);
+    }
+    finally{
+      setLoading(false);
+    }
+  };
+
+  const loadCities = async () => {
     try{
     const storedCities = await AsyncStorage.getItem('savedCity');
     if(storedCities){
-      setSavedCity(JSON.parse(storedCities));
+      const cityName = JSON.parse(storedCities)
+      .filter((item) => item && item.name)
+      .map((item)=>item.name);
+
+      if(cityName.length > 0)
+      fetchWeatherData(cityName)
+      else
+      {
+        console.warn("No valid city names found in stored data.");
+        setLoading(false);
+      }
     }
    }
   catch(error){
     console.error("Error loading cities on focus:", error);
   }
 }
+
+  useEffect(() => {
    loadCities();
   },[]);
 
   useFocusEffect(
     React.useCallback(() => {
-      const loadCities = async () => {
-        try{
-          const storedCities = await AsyncStorage.getItem('savedCity');
-          if(storedCities){
-            console.log(JSON.parse(storedCities));
-            setSavedCity(JSON.parse(storedCities));
-          }
-         }
-        catch(error){
-          console.error("Error loading cities on focus:", error);
-        }
-      };
       loadCities();
     }, [])
   );
@@ -45,6 +66,9 @@ export default function Home({navigation}) {
     
     <Icon name='city' size={30} color='white' style={styles.addCity} onPress={() => navigation.navigate("Search")}/>
       <View>
+      {loading ? (
+          <Text style={{ color: 'white', textAlign: 'center', marginTop: 50 }}>Loading...</Text>
+        ) : (
         <FlatList
         data={savedCity}
         keyExtractor={(item, index) => index.toString()}
@@ -56,7 +80,7 @@ export default function Home({navigation}) {
                     }}
                     style={styles.weatherIcon}
                   />
-                  <View style={{ flex: 1 }}>
+                  <View style={{ flex: 1, marginTop:10 }}>
                     <Text style={styles.cityName}>{item.name}</Text>
                     <Text style={{ color: 'white' }}>{item.main.feels_like}</Text>
                   </View>
@@ -64,6 +88,7 @@ export default function Home({navigation}) {
                 </TouchableOpacity>
   )}
         />
+)}
       </View>
     </View>
   )
@@ -93,7 +118,7 @@ const styles = StyleSheet.create({
    },
    cityName:{
     color:'white',
-    fontSize:25
+    fontSize:20
    },
    tempText:{
     top:15,
